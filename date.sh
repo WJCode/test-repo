aa9eeda9029d539cf66aa9e52fa5b0027e66703a
WJCode <jmw.it50@gmail.com>
2016-01-14 23:02:50 +0530

    subject: add date alone line to script
    this change adds a date alone line to the existing script

--- date.sh
+++ date.sh
@@ -1,3 +1,6 @@
+# these line display today's date and the current time
 echo "Today's date is: "
 date +"%d-%b-%Y %l:%M %p"
 
+# display the date alone in one line
+echo "Today's date is" $(date +"%d-%b-%Y")
